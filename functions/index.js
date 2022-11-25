40340b23c66ed24502c890ca60c58a824c59736c
Bhavuk <Bhavuk.arora03@gmail.com>
2022-11-25 17:53:31 +0530

    cloud functions sucksess. 5.0

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,28 @@
 const functions = require("firebase-functions");
-
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.scheduledFunction = functions.pubsub.schedule("every 5 minutes").onRun((context) =>{
-  console.log("This will be run every 5 minutes!");
-  return null;
-});
+
+const toExactMinute = 1000 * 60 * 60 * 24;
+// eslint-disable-next-line require-jsdoc
+function updateDoc(id) {
+  admin.firestore()
+      .collection("users")
+      .doc(id)
+      .update({thirtyMinDone: false})
+      .then(() => {
+        console.log("Document ID: "+id+" successfully updated!");
+      })
+      .catch((error) => {
+        console.error("Error updating document: ", error);
+      });
+}
+
+exports.updateField = functions.firestore
+    .document("/users/{id}")
+    .onUpdate((snapshot, context) => {
+      console.log("updated success");
+      const id = context.params.id;
+      setTimeout(() => updateDoc(id), toExactMinute);
+      return id;
+    });
